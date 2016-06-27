d28226c7bae8b3a505f92a5560858a116a2b6444
fakecoder <zpfakecoder@gmail.com>
2016-06-27 18:16:04 +0800

    another user

--- app/src/main/java/com/ruijin/myproject/MainActivity.java
+++ app/src/main/java/com/ruijin/myproject/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends AppCompatActivity
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action fdsfddddd", null).show();
+                        .setAction("Action fdsfddddd就离开就离开", null).show();
             }
 
 
