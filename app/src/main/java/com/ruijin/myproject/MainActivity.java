27bf659a16041b726fb7f4dde1b3c86bf6cbcfe9
liangcheng <chengliang0831@163.com>
2016-06-27 17:17:33 +0800

    thrid version new

--- app/src/main/java/com/ruijin/myproject/MainActivity.java
+++ app/src/main/java/com/ruijin/myproject/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends AppCompatActivity
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action fdsf", null).show();
+                        .setAction("Action fdsfddddd", null).show();
             }
 
 
